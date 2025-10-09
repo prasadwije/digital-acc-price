210045906747ee7bfd9aadd451ab31608c428d70
prasadwije <prasadsandaru55@gmail.com>
2025-10-09 09:18:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const DATA_URL = 'https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLhkPngW2qPDpIiY0Sbh7NDt-Fx0t2AgPXaLPFJWLHOEI46Q38fKS7SmV9pmriR9qnf_Sg_pDb2SRPes2Imndyx_JBP8fnD65MubNk8GYqjY2rtoFRbVXuNfxsIlzyp6ggi8UASIT0RafktLrg7ri2GSewBchU0JzngSf_HgUZj7ZopAHMp-qBqVRrWCb4BRhYAV9ogk4twgMMIKIgLLf_8bAahgzzLzcBtd0wcNKT_LMBF4CTWIrZDp8yp6WlWKk81zGfZ55ZsCQhdVTtZRi3iZ9RuHhuEqx-xq8qIZ&lib=MqHVIVDbe7GGrYzAr3eXPOg6Ct9MHB8JG';
+    const DATA_URL = 'https://script.google.com/macros/s/AKfycbyefFSmfSyLRqrQOoTbv5dKT0ncljBJs_uN-KHka98ZnUc9IoYvrLBDkFyII1-7ScS89A/exec';
     const CACHE_KEY = 'digitalPriceCache';
     const CACHE_EXPIRY = 3600000; // 1 hour in milliseconds
 
@@ -202,3 +202,4 @@ function showToast(message) {
         }, 3000);
     }
 }
+
